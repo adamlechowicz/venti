5caaa903b1affb7221d97e6e59493ef9cc1a6bf9
alechowicz <alechowicz@umass.edu>
2023-02-10 13:49:39 -0500

    fix updating mechanism

--- update.sh
+++ update.sh
@@ -8,23 +8,29 @@ tempfolder=~/.venti-tmp
 binfolder=/usr/local/bin
 ventifolder="$tempfolder/venti"
 mkdir -p $ventifolder
+configfolder=/Users/$calling_user/.venti
+thresholdfile=$configfolder/thresholds.conf
 
 echo -e "🔋 Starting Venti update\n"
 
 # Write as executable
 
-echo "[ 1/3 ] Cloning repository"
+echo "[ 1/4 ] Cloning repository"
 rm -rf $ventifolder
 git clone --depth 1 https://github.com/adamlechowicz/venti.git $ventifolder &> /dev/null
-echo "[ 2/3 ] Writing script to $binfolder/venti"
+echo "[ 2/4 ] Writing script to $binfolder/venti"
 cp $ventifolder/venti.sh $binfolder/venti
 chown $USER $binfolder/venti
 chmod 755 $binfolder/venti
 chmod u+x $binfolder/venti
 
+echo "[ 3/4 ] Updating configuration..."
+cp $ventifolder/thresholds.conf $thresholdfile
+chown $USER $thresholdfile
+
 # Remove tempfiles
 cd
 rm -rf $tempfolder
-echo "[ 3/3 ] Removed temporary folder"
+echo "[ 4/4 ] Removed temporary folder"
 
 echo -e "\n🎉 Venti updated.\n"
